8f4144baee3bd3b526e1692f46f90cd39317601d
Donn Lee <git@pluza.com>
2016-12-16 17:24:42 -0800

    Implemented envtpl. Better than sed for modifying .conf files

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM tutum/curl:trusty
 MAINTAINER Donn Lee <docker@pluza.com>
 
+RUN apt-get update -y \
+    && apt-get install -y \
+      curl unzip python-pip \
+    && pip install envtpl
+
 # Install InfluxDB
 ENV INFLUXDB_VERSION 0.12.2-1
 RUN curl -s -o /tmp/influxdb_latest_amd64.deb https://s3.amazonaws.com/influxdb/influxdb_${INFLUXDB_VERSION}_amd64.deb && \
@@ -9,7 +14,8 @@ RUN curl -s -o /tmp/influxdb_latest_amd64.deb https://s3.amazonaws.com/influxdb/
   rm -rf /var/lib/apt/lists/*
 
 ADD types.db /usr/share/collectd/types.db
-ADD config.toml /config/config.toml
+#ADD config.toml /config/config.toml
+ADD influxdb.conf.tpl /config/influxdb.conf.tpl
 ADD run.sh /run.sh
 RUN chmod +x /*.sh
 
--- run.sh
+++ run.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
 set -m
-CONFIG_FILE="/config/config.toml"
+#CONFIG_FILE="/config/config.toml"
+# Name of .conf file AFTER envtpl is done modifying it.
+CONFIG_FILE="/config/influxdb.conf.tpl"
 INFLUX_HOST="localhost"
 INFLUX_API_PORT="8086"
 API_URL="http://${INFLUX_HOST}:${INFLUX_API_PORT}"
 
+# Add Collectd support
+envtpl ${CONFIG_FILE}
+### Orig before envtpl was:
+#if [ -n "${COLLECTD_DB}" ]; then
+#    echo "COLLECTD_DB: ${COLLECTD_DB}"
+#    sed -i -r -e "/^\[collectd\]/, /^$/ { s/false/true/; s/( *)# *(.*)\"collectd\"/\1\2\"${COLLECTD_DB}\"/g;}" ${CONFIG_FILE}
+#fi
+#if [ -n "${COLLECTD_BINDING}" ]; then
+#    echo "COLLECTD_BINDING: ${COLLECTD_BINDING}"
+#    sed -i -r -e "/^\[collectd\]/, /^$/ { s/( *)# *(.*)\":25826\"/\1\2\"${COLLECTD_BINDING}\"/g;}" ${CONFIG_FILE}
+#fi
+#if [ -n "${COLLECTD_RETENTION_POLICY}" ]; then
+#    echo "COLLECTD_RETENTION_POLICY: ${COLLECTD_RETENTION_POLICY}"
+#    sed -i -r -e "/^\[collectd\]/, /^$/ { s/( *)# *(retention-policy.*)\"\"/\1\2\"${COLLECTD_RETENTION_POLICY}\"/g;}" ${CONFIG_FILE}
+#fi
+
+# donn: In all prev servers, max-open-shards was absent from .conf
 # Dynamically change the value of 'max-open-shards' to what 'ulimit -n' returns
-sed -i "s/^max-open-shards.*/max-open-shards = $(ulimit -n)/" ${CONFIG_FILE}
+#sed -i "s/^max-open-shards.*/max-open-shards = $(ulimit -n)/" ${CONFIG_FILE}
 
 # Configure InfluxDB Cluster
-if [ -n "${FORCE_HOSTNAME}" ]; then
-    if [ "${FORCE_HOSTNAME}" == "auto" ]; then
-        #set hostname with IPv4 eth0
-        HOSTIPNAME=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
-        /usr/bin/perl -p -i -e "s/hostname = \"localhost\"/hostname = \"${HOSTIPNAME}\"/g" ${CONFIG_FILE}
-    else
-        /usr/bin/perl -p -i -e "s/hostname = \"localhost\"/hostname = \"${FORCE_HOSTNAME}\"/g" ${CONFIG_FILE}
-    fi
-fi
+#if [ -n "${FORCE_HOSTNAME}" ]; then
+#    if [ "${FORCE_HOSTNAME}" == "auto" ]; then
+#        #set hostname with ip address of eth0
+#        HOSTIPNAME=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
+#        /usr/bin/perl -p -i -e "s/hostname = \"localhost\"/hostname = \"${HOSTIPNAME}\"/g" ${CONFIG_FILE}
+#    else
+#        /usr/bin/perl -p -i -e "s/hostname = \"localhost\"/hostname = \"${FORCE_HOSTNAME}\"/g" ${CONFIG_FILE}
+#    fi
+#fi
 
 # NOTE: 'seed-servers.' is nowhere to be found in config.toml, this cannot work anymore! NEED FOR REVIEW!
 # if [ -n "${SEEDS}" ]; then
@@ -26,9 +45,9 @@ fi
 #     /usr/bin/perl -p -i -e "s/^# seed-servers.*$/seed-servers = [${SEEDS}]/g" ${CONFIG_FILE}
 # fi
 
-if [ -n "${REPLI_FACTOR}" ]; then
-    /usr/bin/perl -p -i -e "s/replication-factor = 1/replication-factor = ${REPLI_FACTOR}/g" ${CONFIG_FILE}
-fi
+#if [ -n "${REPLI_FACTOR}" ]; then
+#    /usr/bin/perl -p -i -e "s/replication-factor = 1/replication-factor = ${REPLI_FACTOR}/g" ${CONFIG_FILE}
+#fi
 
 if [ "${PRE_CREATE_DB}" == "**None**" ]; then
     unset PRE_CREATE_DB
@@ -45,57 +64,44 @@ fi
 # fi
 
 # Add Graphite support
-if [ -n "${GRAPHITE_DB}" ]; then
-    echo "GRAPHITE_DB: ${GRAPHITE_DB}"
-    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/false/true/; s/\"graphitedb\"/\"${GRAPHITE_DB}\"/g; }" ${CONFIG_FILE}
-fi
-
-if [ -n "${GRAPHITE_BINDING}" ]; then
-    echo "GRAPHITE_BINDING: ${GRAPHITE_BINDING}"
-    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/\:2003/${GRAPHITE_BINDING}/; }" ${CONFIG_FILE}
-fi
-
-if [ -n "${GRAPHITE_PROTOCOL}" ]; then
-    echo "GRAPHITE_PROTOCOL: ${GRAPHITE_PROTOCOL}"
-    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/tcp/${GRAPHITE_PROTOCOL}/; }" ${CONFIG_FILE}
-fi
-
-if [ -n "${GRAPHITE_TEMPLATE}" ]; then
-    echo "GRAPHITE_TEMPLATE: ${GRAPHITE_TEMPLATE}"
-    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/instance\.profile\.measurement\*/${GRAPHITE_TEMPLATE}/; }" ${CONFIG_FILE}
-fi
-
-# Add Collectd support
-if [ -n "${COLLECTD_DB}" ]; then
-    echo "COLLECTD_DB: ${COLLECTD_DB}"
-    sed -i -r -e "/^\[collectd\]/, /^$/ { s/false/true/; s/( *)# *(.*)\"collectd\"/\1\2\"${COLLECTD_DB}\"/g;}" ${CONFIG_FILE}
-fi
-if [ -n "${COLLECTD_BINDING}" ]; then
-    echo "COLLECTD_BINDING: ${COLLECTD_BINDING}"
-    sed -i -r -e "/^\[collectd\]/, /^$/ { s/( *)# *(.*)\":25826\"/\1\2\"${COLLECTD_BINDING}\"/g;}" ${CONFIG_FILE}
-fi
-if [ -n "${COLLECTD_RETENTION_POLICY}" ]; then
-    echo "COLLECTD_RETENTION_POLICY: ${COLLECTD_RETENTION_POLICY}"
-    sed -i -r -e "/^\[collectd\]/, /^$/ { s/( *)# *(retention-policy.*)\"\"/\1\2\"${COLLECTD_RETENTION_POLICY}\"/g;}" ${CONFIG_FILE}
-fi
+#if [ -n "${GRAPHITE_DB}" ]; then
+#    echo "GRAPHITE_DB: ${GRAPHITE_DB}"
+#    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/false/true/; s/\"graphitedb\"/\"${GRAPHITE_DB}\"/g; }" ${CONFIG_FILE}
+#fi
+#
+#if [ -n "${GRAPHITE_BINDING}" ]; then
+#    echo "GRAPHITE_BINDING: ${GRAPHITE_BINDING}"
+#    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/\:2003/${GRAPHITE_BINDING}/; }" ${CONFIG_FILE}
+#fi
+#
+#if [ -n "${GRAPHITE_PROTOCOL}" ]; then
+#    echo "GRAPHITE_PROTOCOL: ${GRAPHITE_PROTOCOL}"
+#    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/tcp/${GRAPHITE_PROTOCOL}/; }" ${CONFIG_FILE}
+#fi
+#
+#if [ -n "${GRAPHITE_TEMPLATE}" ]; then
+#    echo "GRAPHITE_TEMPLATE: ${GRAPHITE_TEMPLATE}"
+#    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/instance\.profile\.measurement\*/${GRAPHITE_TEMPLATE}/; }" ${CONFIG_FILE}
+#fi
 
 # Add UDP support
-if [ -n "${UDP_DB}" ]; then
-    sed -i -r -e "/^\[\[udp\]\]/, /^$/ { s/false/true/; s/#//g; s/\"udpdb\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
-fi
-if [ -n "${UDP_PORT}" ]; then
-    sed -i -r -e "/^\[\[udp\]\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
-fi
+#if [ -n "${UDP_DB}" ]; then
+#    sed -i -r -e "/^\[\[udp\]\]/, /^$/ { s/false/true/; s/#//g; s/\"udpdb\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
+#fi
+#if [ -n "${UDP_PORT}" ]; then
+#    sed -i -r -e "/^\[\[udp\]\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
+#fi
 
 
 echo "influxdb configuration: "
 cat ${CONFIG_FILE}
 echo "=> Starting InfluxDB ..."
-if [ -n "${JOIN}" ]; then
-  exec influxd -config=${CONFIG_FILE} -join ${JOIN} &
-else
-  exec influxd -config=${CONFIG_FILE} &
-fi
+#if [ -n "${JOIN}" ]; then
+#  exec influxd -config=${CONFIG_FILE} -join ${JOIN} &
+#else
+#  exec influxd -config=${CONFIG_FILE} &
+#fi
+exec influxd -config=${CONFIG_FILE} &
 
 # Pre create database on the initiation of the container
 if [ -n "${PRE_CREATE_DB}" ]; then
